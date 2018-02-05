02f96e3e723eb0a65afa0eed88a61029ed02c49c
icesmooth2010 <32586621+icesmooth2010@users.noreply.github.com>
2018-02-04 19:00:11 -0700

    addedsomethingincss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -56,14 +56,14 @@ document.onkeyup = function(event) {
             if (userGuess == letterToGuess){
                 wins++;
                 document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes, you are psychic!");
+                alert("Congrats!, you are psychic!");
                 reset();
             }
         }else if(guessesLeft == 0){
             // Then we will loss and we'll update the html to display the loss 
             losses++;
             document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you're not psychic, maybe try again?");
+            alert("Sorry, you're not psychic this time, I think you should try again?");
             // Then we'll call the reset. 
             reset();
         }
